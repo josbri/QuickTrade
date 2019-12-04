4ef24d2b535eda735ff115726a91beddcd36d6bb
Josep <bria.josep@gmail.com>
2019-12-04 18:14:59 +0100

    Ae7

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,12 +8,26 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   categoria : string = " ";
+  img : string= " ";
   
   constructor() { }
 
   //Le pasamos el $Event, y de ahí leemos el detail.value,
   //que es el valor del evento al que se ha cambiado.
+  //Cambiamos la imagen también.
   changeSelected(selectedValue : any){
     this.categoria = selectedValue.detail.value;
+    if (this.categoria == "Hogar"){
+      this.img = "home";
+    } 
+    else if (this.categoria == "Tecnologia"){
+      this.img = "laptop";
+    }
+    else if (this.categoria == "Motor"){
+      this.img = "car";
+    }
+    else if (this.categoria = "Inmobiliaria"){
+      this.img = "business";
+    }
   }
 }
